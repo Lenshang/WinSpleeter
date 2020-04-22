75f2a54f070bb6f2b3eb40188c7b502d1c79f7a7
chen <kagaminele@qq.com>
2020-04-22 18:29:30 +0800

    update

---
+++ Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace WinSpleeter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbFilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rb2stems = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rb4stems = new System.Windows.Forms.RadioButton();
+            this.rb5stems = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btOpen = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(387, 417);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(694, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbFilePath
+            // 
+            this.tbFilePath.Location = new System.Drawing.Point(405, 55);
+            this.tbFilePath.Name = "tbFilePath";
+            this.tbFilePath.ReadOnly = true;
+            this.tbFilePath.Size = new System.Drawing.Size(283, 21);
+            this.tbFilePath.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(400, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "1.Choose a audio File";
+            // 
+            // rb2stems
+            // 
+            this.rb2stems.AutoSize = true;
+            this.rb2stems.Checked = true;
+            this.rb2stems.Location = new System.Drawing.Point(407, 135);
+            this.rb2stems.Name = "rb2stems";
+            this.rb2stems.Size = new System.Drawing.Size(59, 16);
+            this.rb2stems.TabIndex = 4;
+            this.rb2stems.TabStop = true;
+            this.rb2stems.Text = "2stems";
+            this.rb2stems.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(402, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(172, 28);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "2.Choose Stems";
+            // 
+            // rb4stems
+            // 
+            this.rb4stems.AutoSize = true;
+            this.rb4stems.Location = new System.Drawing.Point(472, 135);
+            this.rb4stems.Name = "rb4stems";
+            this.rb4stems.Size = new System.Drawing.Size(59, 16);
+            this.rb4stems.TabIndex = 6;
+            this.rb4stems.Text = "4stems";
+            this.rb4stems.UseVisualStyleBackColor = true;
+            // 
+            // rb5stems
+            // 
+            this.rb5stems.AutoSize = true;
+            this.rb5stems.Location = new System.Drawing.Point(537, 135);
+            this.rb5stems.Name = "rb5stems";
+            this.rb5stems.Size = new System.Drawing.Size(59, 16);
+            this.rb5stems.TabIndex = 7;
+            this.rb5stems.Text = "5stems";
+            this.rb5stems.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(405, 209);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(362, 57);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Process";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(400, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(239, 28);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "3.Press Process Button";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(400, 350);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(261, 28);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "3.Open Output Directory";
+            // 
+            // btOpen
+            // 
+            this.btOpen.Enabled = false;
+            this.btOpen.Font = new System.Drawing.Font("微软雅黑", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btOpen.Location = new System.Drawing.Point(405, 393);
+            this.btOpen.Name = "btOpen";
+            this.btOpen.Size = new System.Drawing.Size(362, 57);
+            this.btOpen.TabIndex = 10;
+            this.btOpen.Text = "Open";
+            this.btOpen.UseVisualStyleBackColor = true;
+            this.btOpen.Click += new System.EventHandler(this.btOpen_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(405, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "选择一个音频文件";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(407, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(221, 12);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "选择音频分离程度(消除人声默认2stems)";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(405, 191);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 12);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "点击Process按钮";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(405, 378);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(185, 12);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "点击Open按钮打开处理结果文件夹";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label9.Location = new System.Drawing.Point(402, 288);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(238, 28);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "4.Wait For Completion";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(405, 316);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(77, 12);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "等待左侧完成";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 462);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btOpen);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.rb5stems);
+            this.Controls.Add(this.rb4stems);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.rb2stems);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbFilePath);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.MaximumSize = new System.Drawing.Size(797, 501);
+            this.MinimumSize = new System.Drawing.Size(797, 501);
+            this.Name = "Form1";
+            this.Text = "WinSpleeter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbFilePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rb2stems;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton rb4stems;
+        private System.Windows.Forms.RadioButton rb5stems;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btOpen;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinSpleeter
+{
+    public partial class Form1 : Form
+    {
+        RunnerModule runner;
+        String openPath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.SetOut(new TextBoxWriter(this.textBox1));
+        }
+
+        private void StartProcess(string filePath,Action callback,string stems="2stems")
+        {
+            Console.WriteLine("Spleeter start!");
+            this.runner = new RunnerModule("miniconda3/Scripts/activate.bat", $"./miniconda3 && python -m spleeter separate -i \"{filePath}\" -p spleeter:{stems} -o output", false);
+            this.runner.OnMessageReceive = (string msg) => {
+                Console.WriteLine(msg);
+            };
+            this.runner.OnProcessExit = () =>
+            {
+                Console.WriteLine("Spleeter exited");
+                callback();
+            };
+            this.runner.Run();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;//该值确定是否可以选择多个文件
+            dialog.Title = "请选择文件";
+            dialog.Filter = "MP3文件(*.mp3)|*.mp3";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string file = dialog.FileName;
+                this.tbFilePath.Text = file;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.tbFilePath.Text))
+            {
+                MessageBox.Show("尚未选择文件!");
+                return;
+            }
+
+            if (!File.Exists(this.tbFilePath.Text))
+            {
+                MessageBox.Show("选择的文件不存在!");
+                return;
+            }
+
+            (sender as Button).Enabled = false;
+            string selectStems = "2stems";
+            if (this.rb4stems.Checked)
+            {
+                selectStems = "4stems";
+            }
+            else if (this.rb5stems.Checked)
+            {
+                selectStems = "5stems";
+            }
+
+            Action callback = () => {
+                this.Invoke(new Action(() => {
+                    MessageBox.Show("处理完成");
+                    (sender as Button).Enabled = true;
+                    this.btOpen.Enabled = true;
+                }));
+            };
+
+            this.StartProcess(this.tbFilePath.Text, callback, selectStems);
+        }
+
+        private void btOpen_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists("./output"))
+            {
+                System.Diagnostics.Process.Start("explorer", Path.Combine(System.Environment.CurrentDirectory, "output"));
+            }
+        }
+    }
+}
